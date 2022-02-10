2598d8cad0291e26b162255801e52288df6e4031
niko-38500 <nicolas.montmayeur@hotmail.fr>
2022-02-10 20:24:22 +0100

    add a model for the design pattern decorator

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,83 @@
-import Kernel from "./core/kernel.js";
-import Router from "./core/routing/router.js";
-import Route from "./core/routing/route.js";
+// import Kernel from "./core/kernel.js";
+// import Router from "./core/routing/router.js";
+// import Route from "./core/routing/route.js";
+//
+// const kernel = new Kernel();
+// kernel.initApp();
+//
+// const router = Router.getInstance([
+//     // TODO : put your routes here
+// ]);
+//
+// // router!.navigate(url);
+//
+// const url = window.location.pathname;
+//
+// window.addEventListener('popstate', () => {
+//     router!.navigate(url);
+// });
 
-const kernel = new Kernel();
-kernel.initApp();
 
-const router = Router.getInstance([
-    // TODO : put your routes here
-]);
 
-const url = window.location.pathname;
 
-// router!.navigate(url);
 
-window.addEventListener('popstate', () => {
-    const url = window.location.pathname;
-    router!.navigate(url);
-});
+
+
+
+
+
+//////////////////////////////////////   Decorator pattern must be implemented   ///////////////////////////////////////
+
+//
+// type a = {
+//     htmlFile?:string,
+//     cssFile?: string,
+//     template?: string,
+// }
+//
+// function Component(options: a) {
+//     return function (target: typeof aa) {
+//         if (options['template']) {
+//             document.body.innerHTML = options['template'];
+//         }
+//     }
+// }
+//
+//
+//
+//
+//
+//
+//
+// @Component({
+//     htmlFile: "index.html",
+//     cssFile: 'index.css',
+//     template: '<p>mlfjkekmfj</p><br><h2>Secondary Title</h2>'
+// })
+// class aa {
+//     t() {
+//         console.log("msldfk")
+//     }
+// }
+//
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
