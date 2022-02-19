cad44b2b380f938481c2011acc3ddedb7ffe87f6
niko-38500 <nicolas.montmayeur@hotmail.fr>
2022-02-19 12:34:40 +0100

    try to resolve path locally

--- cli/jsf.js
+++ cli/jsf.js
@@ -4,42 +4,76 @@ const [, , ...args] = process.argv;
 const fs = require('fs');
 const path = require("path");
 
-const findFilesRecursively = (directory, listOfFile, recursiveFolder = '') => {
-    const resolvedPath = path.resolve('cli', directory);
-    const dir = fs.readdirSync(resolvedPath);
-
-    dir.forEach((item) => {
-        if (fs.lstatSync(item).isDirectory()) {
-            return findFilesRecursively(path.join(directory, item), listOfFile, item);
-        }
-
-        const fileWithPath = '' !== recursiveFolder
-            ? `${recursiveFolder}/${item}`
-            : item
+const // utils closures
+    findFilesRecursively = (currentPath, listOfFile) => {
+        const
+            resolvedPath = path.resolve(__dirname, currentPath),
+            dir = fs.readdirSync(resolvedPath)
         ;
 
-        listOfFile.push(fileWithPath);
-    });
-};
+        dir.forEach((item) => {
+            const
+                newPath = path.join(currentPath, item),
+                isDirectory = fs.statSync(path.join(__dirname, newPath)).isDirectory()
+            ;
 
-const // CLI command
+            if (isDirectory) {
+                return findFilesRecursively(newPath, listOfFile);
+            }
+
+            listOfFile.push(newPath);
+        });
+    },
+
+    deploy = (boilerplateFiles) => {
+        console.warn('\x1b[36m', 'Generate files');
+
+        boilerplateFiles.forEach((file) => {
+            const
+                projectPath = path.resolve('./'),
+                t = file.split('/').slice(1).join('/'),
+                currentFile = path.resolve(__dirname, file)
+            ;
+
+            if (file.split('/')) {
+                console.log('splittttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttttt');
+            }
+
+            console.log(t);
+
+            // fs.copyFileSync(currentFile, path.join(projectPath, t));
+
+            console.log('\x1b[32m', `${file} successfully created ...`, '\u2713', '\x1b[0m');
+        });
+
+        console.log(`your project ${args[1]} is successfully created`);
+    }
+;
+
+const // CLI available command
     NEW = 'new',
-    GENERATE = 'generate'
+    GENERATE = 'generate',
+    INIT = 'init'
 ;
 
 (function cli() {
     if (NEW === args[0]) {
+        if (!args[1]) {
+            console.error('please provide a project name "example: <jsf new my_new_app>"');
+            return;
+        }
+        const PROJECT_NAME = args[1];
         const boilerplateFiles = [];
 
-        findFilesRecursively('boilerplate', boilerplateFiles);
+        findFilesRecursively('boilerplate/', boilerplateFiles);
+        deploy(boilerplateFiles);
 
-        boilerplateFiles.forEach((file) => {
-            fs.copyFileSync(path.join(__dirname, file), file);
-        });
         return;
     } else if (GENERATE === args[0]) {
         console.log('must be implemented');
         return;
+    } else if (INIT === args[0]) {
+
     }
 
     console.warn('please provide a valid argument');
---
+++ src/index.d.ts
@@ -0,0 +1,4 @@
+declare module 'js-framework';
+export { Router } from './core/routing/router';
+export { Route } from './core/routing/route';
+export { ViewModel } from './core/component/view.model';
